fed409c333eed701e470a575a4b955c969b0bc96
Soumya Sambeet Mohapatra <sambeet96@gmail.com>
2021-11-27 10:44:13 +0100

    Added tries count and hints

--- guess.py
+++ guess.py
@@ -2,16 +2,20 @@
 import random
 
 number = random.randint(1, 100)
+tries = 1
+
 print("Welcome to Guess the Number")
 print("===========================")
 print("I've thought of a number between 1 to 100. Try to guess it in as few tries as possible.")
 print(number)
 
 guess = int(input("Your guess:\t"))
-while(guess != number):
-    guess = int(input("Guess again:\t"))
 
-print("Congratulations! You guessed it right. The number is {}".format(number))
-print("================")
-print("Have a nice day!")
-print("================")
+while(guess != number):
+    if(guess < number):
+        guess = int(input("Try guessing a higher number:\t"))
+    else:
+        guess = int(input("Try guessing a lower number:\t"))
+    tries += 1
+
+print("Congratulations! You guessed the number in {} tries. The number is {}".format(tries, number))
